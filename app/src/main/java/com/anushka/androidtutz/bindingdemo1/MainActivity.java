fd952895b087febde508f7b729fb2efcaf88e9bf
SinaSystem <sinasystem47@gmail.com>
2020-06-04 23:49:36 +0430

    Initial commit

---
+++ app/src/main/java/com/anushka/androidtutz/bindingdemo1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.anushka.androidtutz.bindingdemo1;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.anushka.androidtutz.bindingdemo1.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        activityMainBinding=DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding.setStudent(getCurrentStudent());
+
+    }
+
+    private Student getCurrentStudent(){
+
+        Student student = new Student();
+        student.setStudentName("Alex");
+        student.setStudentEmail("alex@gmail.com");
+        return student;
+
+    }
+}
